f8fe22dc64f7c46d94b7923d5592fd7c3073a154
DeimGuce <gucedi@students.nu-lipa.edu.ph>
2026-05-02 21:28:38 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,23 @@
 <?php
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\LegacyMobileApiController;
 use Illuminate\Support\Facades\Route;
 
-// Iwanan itong file na ito kung may ibang API routes ka, 
-// pero siguraduhin na wala dito yung generate-caption.
+Route::post('/login.php', [LegacyMobileApiController::class, 'login']);
+Route::post('/register.php', [LegacyMobileApiController::class, 'register']);
+Route::post('/otp_verify.php', [LegacyMobileApiController::class, 'verifyOtp']);
+Route::post('/resend_otp.php', [LegacyMobileApiController::class, 'resendOtp']);
+Route::get('/profile.php', [LegacyMobileApiController::class, 'profile']);
+Route::get('/requests.php', [LegacyMobileApiController::class, 'requests']);
+Route::post('/create_request.php', [LegacyMobileApiController::class, 'createRequest']);
+Route::post('/generate_caption.php', [LegacyMobileApiController::class, 'generateCaption']);
+Route::get('/calendar.php', [LegacyMobileApiController::class, 'calendar']);
+Route::post('/update_profile.php', [LegacyMobileApiController::class, 'updateProfile']);
+Route::post('/update_password.php', [LegacyMobileApiController::class, 'updatePassword']);
+Route::get('/notifications.php', [LegacyMobileApiController::class, 'notifications']);
+Route::post('/mark_notification_read.php', [LegacyMobileApiController::class, 'markNotificationRead']);
+Route::post('/mark_messages_read.php', [LegacyMobileApiController::class, 'markMessagesRead']);
+Route::get('/request_details.php', [LegacyMobileApiController::class, 'requestDetails']);
+Route::get('/messages.php', [LegacyMobileApiController::class, 'messageThreads']);
+Route::get('/message_thread.php', [LegacyMobileApiController::class, 'messageThread']);
+Route::post('/message_thread.php', [LegacyMobileApiController::class, 'sendMessage']);
